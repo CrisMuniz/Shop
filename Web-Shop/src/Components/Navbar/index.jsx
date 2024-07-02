d66637928ab3454d0bcdce273644fca00c01f0d1
CrisMuniz <cristinamuniz85@gmail.com>
2024-07-02 13:27:46 +0200

    add Navbar component with his diferents links

---
+++ Web-Shop/src/Components/Navbar/index.jsx
@@ -0,0 +1,83 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav className="flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light ">
+            {/* Lado izquierdo del Navbar */}
+            <ul className="flex items-center gap-3">
+                <li className="font-semibold text-lg">
+                    <NavLink
+                        to='/'>
+                            YoyoShop
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/'>
+                            All
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/clothes'>
+                            Clothes
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/electronics'>
+                            Eletronics
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/furnitures'>
+                            Furnitures
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/toys'>
+                            Toys
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/others'>
+                            Others
+                    </NavLink>
+                </li>
+            </ul>
+            {/* Lado derecho del Navbar */}
+            <ul className="flex items-center gap-3">
+                <li>
+                    crisyoyo@gmil.com
+                </li>
+                <li>
+                    <NavLink
+                        to='/my-account'>
+                            My Account
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/my-orders'>
+                            My Orders
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to='/sign-in'>
+                            Sign In
+                    </NavLink>
+                </li>
+                <li>
+                    🛒 0
+                </li>
+
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
