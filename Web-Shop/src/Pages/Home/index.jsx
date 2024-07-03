e6ecd3f4a8b3c306ccadf682fa238cc8811f8e0d
CrisMuniz <cristinamuniz85@gmail.com>
2024-07-03 14:38:56 +0200

    add fetch Api, useState and useEffect, map and props for Card component

--- Web-Shop/src/Pages/Home/index.jsx
+++ Web-Shop/src/Pages/Home/index.jsx
@@ -1,12 +1,25 @@
+import { useState, useEffect } from "react";
 import Layout from "../../Components/Layout";
 import Card from "../../Components/Card";
 
 function Home() {
+  const [items, setItems] = useState(null);
+
+    useEffect(() => {
+      fetch('https://api.escuelajs.co/api/v1/products?offset=0&limit=45')
+      .then(response => response.json())
+      .then(data => setItems(data))
+    }, [])
     return (
       <>
        <Layout>
-        <div>Home</div>
-        <Card/>
+        <div className="grid grid-cols-4 gap-y-4  gap-x-2 w-full max-w-screen-lg">
+          {
+            items?.map(item => (
+               <Card key={item.id} data={item}/>
+            ))
+          }
+        </div>
        </Layout>
       </>
     )
