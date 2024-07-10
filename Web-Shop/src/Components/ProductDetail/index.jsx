b1284b3a873e4580bfb7c269b42389e9495b6f36
CrisMuniz <cristinamuniz85@gmail.com>
2024-07-10 19:58:11 +0200

    hide scrollbar to ProductDetail

--- Web-Shop/src/Components/ProductDetail/index.jsx
+++ Web-Shop/src/Components/ProductDetail/index.jsx
@@ -7,7 +7,7 @@ const ProductDetail = () => {
 
     const onCancel = () => {setOpenModal(false)};
     return (
-        <div className="flex flex-col fixed bg-white  rounded-lg w-[300px] h-[calc(100vh-130px)]  overflow-auto  shadow-lg hover:shadow-xl hover:scale-105 duration-500 transform transition scr">
+        <div className="flex flex-col fixed bg-white  rounded-lg w-[300px] h-[calc(100vh-130px)]  overflow-auto no-scrollbar shadow-lg hover:shadow-xl hover:scale-105 duration-500 transform transition">
             <div className="flex justify-between items-center p-6 ">
                 <h2 className="font-medium text-xl">Product Detail</h2>
                 <div onClick={onCancel}><XCircleIcon className="h-6 w-6 cursor-pointer text-black"></XCircleIcon>
