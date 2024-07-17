7e4d982626206f63e64cfb0720a107b71ade7cf8
CrisMuniz <cristinamuniz85@gmail.com>
2024-07-17 13:11:45 +0200

    add styles in OrderCard component

--- Web-Shop/src/Components/OrderCard/index.jsx
+++ Web-Shop/src/Components/OrderCard/index.jsx
@@ -2,15 +2,15 @@ import {XCircleIcon} from '@heroicons/react/24/solid';
 
 const OrderCard = () => {
     return (
-        <div>
-            <div>
-                <figure>
-                    <img src="" alt="" />
+        <div className='flex justify-between items-center'>
+            <div className='flex items-center gap-2'>
+                <figure className='w-20 h-20'>
+                    <img className='w-full h-full rounded-lg object-cover' src="" alt="" />
                 </figure>
-                <p></p>
+                <p className='text-sm font-light'></p>
             </div>
-            <div>
-                <p></p>
+            <div className='flex items-center gap-2'>
+                <p className='text-lg font-medium'></p>
                 <XCircleIcon className="h-6 w-6 cursor-pointer text-black"></XCircleIcon>
             </div>
         </div>
